7924a79d2c7887e2d03d74d0ff37b6ba18269f8d
Garo Kechkarian <garokechkarian@Garos-iMac.local>
2022-06-01 20:54:12 -0700

    Finished

--- index.js
+++ index.js
@@ -13,10 +13,35 @@ content.append(dogContent)
 let dogImage = document.createElement('img')
 dogImage.setAttribute('class', 'dog-image')
 dogImage.setAttribute('src', './assets/rizzo.jpg')
-content.append(dogImage)
+dogContent.append(dogImage)
 
 let dogDetails = document.createElement('div')
 dogDetails.setAttribute('class', 'dog-details')
-content.append(dogDetails)
+dogContent.append(dogDetails)
+
+let headerT = document.createElement ('h3');
+headerT.textContent = 'Description:'
+dogDetails.append(headerT)
+
+let paragraphU = document.createElement ('p')
+paragraphU.textContent = 'This gentle dog is aloof toward her owner, and never comes when called. She always acts as though any stranger she meets will harm her, and dislikes other animals.'
+dogDetails.append(paragraphU)
 
 
+let headerX = document.createElement ('h3')
+headerX.textContent = 'Feeding Times:'
+dogDetails.append(headerX)
+
+
+
+let listA = document.createElement ('li')
+listA.textContent = '9:00 am'
+dogDetails.append(listA)
+
+let listB = document.createElement ('li')
+listB.textContent = '12:00 pm'
+dogDetails.append(listB)
+
+let listC = document.createElement ('li')
+listC.textContent = '5:00 pm'
+dogDetails.append(listC)
